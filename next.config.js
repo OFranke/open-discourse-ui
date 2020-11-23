af1d3bc6bc16d3f9feb90da8b0806faab555566a
Oliver Franke <oliver.franke@limebit.de>
2020-11-23 17:30:37 +0100

    add X-Content-Type-Options

--- next.config.js
+++ next.config.js
@@ -65,6 +65,8 @@ module.exports = extend(envConfig).withPlugins([
             },
             // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
             { key: "X-Frame-Options", value: "SAMEORIGIN" },
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+            { key: "X-Content-Type-Options", value: "nosniff" },
           ],
         },
       ];
