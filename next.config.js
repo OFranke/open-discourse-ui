4cc2e799e0f073a354e9b8f3c86d8f5c8cd6e01e
Oliver Franke <oliver.franke@limebit.de>
2020-11-23 17:28:01 +0100

    add Content Security Policy

--- next.config.js
+++ next.config.js
@@ -52,4 +52,20 @@ module.exports = extend(envConfig).withPlugins([
   ],
 
   // your other plugins here
+  {
+    async headers() {
+      return [
+        {
+          source: "/:path*",
+          headers: [
+            {
+              // // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+              key: "Content-Security-Policy",
+              value: "default-src 'self'",
+            },
+          ],
+        },
+      ];
+    },
+  },
 ]);
