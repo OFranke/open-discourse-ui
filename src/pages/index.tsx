ad8c3eae803dee67d7f0a9c77e9badfc60c66390
Oliver Franke <oliver.franke@limebit.de>
2020-12-14 16:50:21 +0100

    try out static page generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,10 @@ import ConditionallyRender from "../components/conditionally-render";
 import { DefaultListItem } from "../components/default-list-item";
 import { NextChakraLink } from "@bit/limebit.limebit-ui.next-chakra-link";
 
+export const config = {
+  unstable_runtimeJS: false,
+};
+
 const Home: React.FC = () => {
   return (
     <BaseTemplate>
