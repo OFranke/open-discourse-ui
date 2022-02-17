8a0cfae70ad8c474cf936800e9190fdd59db33cb
Oliver Franke <oliver.franke@limebit.de>
2022-02-17 12:18:11 +0100

    change font hosting

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ import Document, {
   NextScript,
   Head,
 } from "next/document";
-import GoogleFonts from "next-google-fonts";
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -16,11 +15,6 @@ class MyDocument extends Document {
     return (
       <Html lang="de">
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,400;0,700;1,400;1,700&family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400;1,700&display=swap"
-            rel="stylesheet"
-          />
           <link
             rel="icon"
             type="image/png"
@@ -45,10 +39,74 @@ class MyDocument extends Document {
           <style
             dangerouslySetInnerHTML={{
               __html: `
-                        body {
-                            font-family: 'Source Code Pro, sans-serif';
-                        }
-                      `,
+                      @font-face {
+                          font-family: 'Source Code Pro';
+                          font-weight: regular;
+                          font-display: auto;
+                          font-style: normal;
+                          src: url('/fonts/SourceCodePro-Regular.ttf');
+                      }
+                      @font-face {
+                          font-family: 'Source Code Pro';
+                          font-weight: bold;
+                          font-display: auto;
+                          font-style: normal;
+                          src: url('/fonts/SourceCodePro-Bold.ttf');
+                      }
+                      @font-face {
+                          font-family: 'Source Code Pro';
+                          font-weight: regular;
+                          font-display: auto;
+                          font-style: italic;
+                          src: url('/fonts/SourceCodePro-Italic.ttf');
+                      }
+                      @font-face {
+                          font-family: 'Source Code Pro';
+                          font-weight: bold;
+                          font-display: auto;
+                          font-style: italic;
+                          src: url('/fonts/SourceCodePro-BoldItalic.ttf');
+                      }
+
+                      @font-face {
+                          font-family: 'Source Sans Pro';
+                          font-weight: regular;
+                          font-display: auto;
+                          font-style: normal;
+                          src: url('/fonts/SourceSansPro-Regular.ttf');
+                      }
+                      @font-face {
+                          font-family: 'Source Sans Pro';
+                          font-weight: bold;
+                          font-display: auto;
+                          font-style: normal;
+                          src: url('/fonts/SourceSansPro-Bold.ttf');
+                      }
+                      @font-face {
+                          font-family: 'Source Sans Pro';
+                          font-weight: regular;
+                          font-display: auto;
+                          font-style: italic;
+                          src: url('/fonts/SourceSansPro-Italic.ttf');
+                      }
+                      @font-face {
+                          font-family: 'Source Sans Pro';
+                          font-weight: 600;
+                          font-display: auto;
+                          font-style: semibold;
+                          src: url('/fonts/SourceSansPro-SemiBold.ttf');
+                      }
+                      @font-face {
+                          font-family: 'Source Sans Pro';
+                          font-weight: bold;
+                          font-display: auto;
+                          font-style: italic;
+                          src: url('/fonts/SourceSansPro-BoldItalic.ttf');
+                      }
+                      body {
+                          font-family: 'Source Code Pro, sans-serif';
+                      }
+                    `,
             }}
           />
         </Head>
